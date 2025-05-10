0ace6e8165e4132e90b3211b1d52fb03e98eea46
dpazj <joepaz200@gmail.com>
2025-05-10 12:28:18 +0100

    wip

--- srad-app/src/simple.rs
+++ srad-app/src/simple.rs
@@ -1,12 +1,24 @@
 use std::{collections::HashMap, sync::{Arc, Mutex}};
 
-use srad_types::MetricId;
+use srad_types::{payload::DataType, MetricId};
 
-use crate::{resequencer::Resequencer, AppEvent, AppEventLoop, NodeIdentifier};
+use crate::{events::{DBirth, DData, DDeath, NBirth, NData, NDeath}, resequencer::{self, Resequencer}, AppEvent, AppEventLoop, MetricBirthDetails, MetricDetails, NodeIdentifier};
 
 
 struct Metric {
+    stale: bool,
+    datatype: DataType
+}
 
+impl Metric {
+
+    fn new(datatype: DataType) -> Self {
+        Self {
+            stale: false,
+            datatype,
+        }
+    }
+    
 }
 
 struct State {
@@ -14,14 +26,54 @@ struct State {
     stale: bool,
 }
 
+impl State {
+
+    fn new() -> Self {
+        Self {
+            metrics: HashMap::new(),
+            stale: true,
+        }
+    }
+
+    fn update_from_birth(&mut self, metrics: Vec<(MetricBirthDetails, MetricDetails)>) {
+
+        self.stale = false;
+
+        self.metrics.clear();
+
+        for (x, y) in metrics {
+
+            let id = if let Some(alias) = x.alias {
+                MetricId::Alias(alias)
+            } else {
+                MetricId::Name(x.name.clone())
+            };
+
+            let metric = Metric::new(x.datatype);
+            self.metrics.insert(id, metric);
+        }
+
+    }
+
+}
+
+enum ResequenceableEvent {
+    NData(NData),
+    DBirth(DBirth),
+    DDeath(DDeath),
+    DData(DData)
+}
+
 struct Device {
     state: State
 }
 
 struct Node {
-    resequencer: Resequencer<AppEvent>,
+    resequencer: Resequencer<ResequenceableEvent>,
     devices: HashMap<NodeIdentifier, Arc<Node>>,
-    state: State
+    state: State,
+    bdseq: u8,
+    last_birth_timestamp: u64
 }
 
 impl Node {
@@ -30,16 +82,60 @@ impl Node {
         Self {
             resequencer: Resequencer::new(),
             devices: HashMap::new(),
-            metrics: todo!(),
+            bdseq: 0,
+            last_birth_timestamp: 0,
+            state: State::new()
         }
+    }
+
+    fn handle_birth(&mut self, details: NBirth){
+
+        if details.timestamp <= self.last_birth_timestamp { return }
+        if self.bdseq == details.bdseq { return }
+        self.bdseq = details.bdseq;
+        self.state.update_from_birth(details.metrics_details);
+        //self.resequencer.reset()
+
+    }
+
+    fn handle_death(&mut self, details: NDeath){
+
+    }
+
+    fn process_resequencable_message(&mut self, message: ResequenceableEvent) {
+
+        match message {
+            ResequenceableEvent::NData(ndata) => todo!(),
+            ResequenceableEvent::DBirth(dbirth) => todo!(),
+            ResequenceableEvent::DDeath(ddeath) => todo!(),
+            ResequenceableEvent::DData(ddata) => todo!(),
+        }
+
+    }
+
+    fn handle_resequencable_message(&mut self, seq: u8, message: ResequenceableEvent) {
+        let message = match self.resequencer.process(seq, message) {
+            resequencer::ProcessResult::MessageNextInSequence(message) => message,
+            resequencer::ProcessResult::OutOfSequenceMessageInserted => return,
+            resequencer::ProcessResult::DuplicateMessageSequence => todo!("rebirth"),
+        };
+
+        self.process_resequencable_message(message);
+
+        loop {
+            match self.resequencer.drain() {
+                resequencer::DrainResult::Message(message) => self.process_resequencable_message(message),
+                resequencer::DrainResult::Empty => break,
+                resequencer::DrainResult::SequenceMissing => break,
+            }
+        }
+
     }
 
 }
 
 struct Application {
-
-    nodes: HashMap<NodeIdentifier, Arc<Node>>,
-
+    nodes: HashMap<NodeIdentifier, Arc<Mutex<Node>>>,
     eventloop: AppEventLoop
 }
 
@@ -49,6 +145,9 @@ impl Application {
 
     }
 
+    fn handle_resequencable_message(&self, seq: u8, message: ResequenceableEvent) {
+    }
+
     pub async fn run(&mut self) {
         loop {
             match self.eventloop.poll().await {
@@ -56,22 +155,69 @@ impl Application {
                 AppEvent::Offline => todo!(),
                 AppEvent::NBirth(nbirth) => {
                     let node = match self.nodes.get(&nbirth.id) {
-                        Some(_) => todo!(),
+                        Some(n) => n.clone(),
                         None => {
-                            let node = Arc::new (Node::new());
-                            self.nodes.insert(nbirth.id, node.clone());
+                            let node = Arc::new (Mutex::new(Node::new()));
+                            self.nodes.insert(nbirth.id.clone(), node.clone());
                             node
                         },
                     };
-
-
+                    tokio::spawn(async move {
+                        let mut n = node.lock().unwrap();
+                        n.handle_birth(nbirth);
+                    });
                 },
-                AppEvent::NDeath(ndeath) => todo!(),
-                AppEvent::NData(ndata) => todo!(),
-                AppEvent::DBirth(dbirth) => todo!(),
-                AppEvent::DDeath(ddeath) => todo!(),
-                AppEvent::DData(ddata) => todo!(),
-                AppEvent::InvalidPayload(payload_error_details) => todo!(),
+                AppEvent::NDeath(ndeath) => {
+                    let node = match self.nodes.get(&ndeath.id) {
+                        Some(node) => node.clone(),
+                        None => continue,
+                    };
+                    tokio::spawn(async move {
+                        let mut n = node.lock().unwrap();
+                        n.handle_death(ndeath);
+                    });
+                },
+                AppEvent::NData(ndata) => {
+                    let node = match self.nodes.get(&ndata.id) {
+                        Some(node) => node.clone(),
+                        None => todo!("issue rebirth"),
+                    };
+                    tokio::spawn(async move {
+                        let mut n = node.lock().unwrap();
+                        n.handle_resequencable_message(ndata.seq, ResequenceableEvent::NData(ndata));
+                    });
+                },
+                AppEvent::DBirth(dbirth) => {
+                    let node = match self.nodes.get(&dbirth.node_id) {
+                        Some(node) => node.clone(),
+                        None => todo!("issue rebirth"),
+                    };
+                    tokio::spawn(async move {
+                        let mut n = node.lock().unwrap();
+                        n.handle_resequencable_message(dbirth.seq, ResequenceableEvent::DBirth(dbirth));
+                    });
+                },
+                AppEvent::DDeath(ddeath) => {
+                    let node = match self.nodes.get(&ddeath.node_id) {
+                        Some(node) => node.clone(),
+                        None => todo!("issue rebirth"),
+                    };
+                    tokio::spawn(async move {
+                        let mut n = node.lock().unwrap();
+                        n.handle_resequencable_message(ddeath.seq, ResequenceableEvent::DDeath(ddeath));
+                    });
+                },
+                AppEvent::DData(ddata) => {
+                    let node = match self.nodes.get(&ddata.node_id) {
+                        Some(node) => node.clone(),
+                        None => todo!("issue rebirth"),
+                    };
+                    tokio::spawn(async move {
+                        let mut n = node.lock().unwrap();
+                        n.handle_resequencable_message(ddata.seq, ResequenceableEvent::DData(ddata));
+                    });
+                },
+                AppEvent::InvalidPayload(_) => (),
                 AppEvent::Cancelled => break,
             }
         }        
